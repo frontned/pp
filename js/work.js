835f391d7f6ae144f1a072ad8d0f6ebd48f254e4
frontned <nedass@wix.com>
2020-09-29 18:52:57 +0300

    Add video, fixes after UX review

--- js/work.js
+++ js/work.js
@@ -2,7 +2,6 @@ const workItems = document.querySelectorAll('.js-work-item')
 
 window.addEventListener('scroll', () => {
   Array.from(workItems).forEach(workItem => {
-    console.log(workItem.getBoundingClientRect().y)
     if (workItem.getBoundingClientRect().y <= 100) {
       workItem.classList.add('animate')
     } else {
